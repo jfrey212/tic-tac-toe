3100c7fe9edb82d9baf5d0abc968ea88d5fcfd14
jfrey212 <eojyerf@gmail.com>
2022-03-22 23:57:11 -0500

    added classes - Player, Board, Game, Turn

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,4 +1,56 @@
 # Odin Project Tic-Tac-Toe Game
 # by Joseph Frey
 
+class Game
+  attr_accessor :name, :number_of_games
 
+  def initialize
+
+  end
+
+  def start_game
+
+  end
+
+  def execute_turn
+
+  end
+
+  def check_for_victory
+
+  end
+
+  def end_game
+end
+
+class Player
+  attr_accessor :name, :number
+
+  def initialize
+
+  end
+
+  def make_move
+
+  end
+end
+
+class Turn
+  attr_accessor :number, :player
+
+  def initialize
+
+  end
+
+  def add_player_move
+
+  end
+end
+
+class Board
+  attr_accessor :name
+
+  def initialize
+
+  end
+end
