cd053e5e7e69adb5713f76ca48de66a1b670d397
jfrey212 <eojyerf@gmail.com>
2022-03-23 01:25:20 -0500

    Built out methods in the Board class

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -2,7 +2,7 @@
 # by Joseph Frey
 
 class Game
-  attr_accessor :name, :number_of_games
+  attr_reader :number_of_games, :name
 
   def initialize
 
@@ -42,15 +42,38 @@ class Turn
 
   end
 
+  def get_move
+
+  end
+
   def add_player_move
 
   end
 end
 
 class Board
-  attr_accessor :name
-
+  
   def initialize
+    @grid = {a1: " ", a2: " ", a3: " ",
+             b1: " ", b2: " ", b3: " ",
+             c1: " ", c2: " ", c3: " "}
+  end
 
+  def add_x(cell)
+    @grid[cell] = "X"
+  end
+
+  def add_o(cell)
+    @grid[cell] = "O"
+  end
+
+  def display_board
+    puts " #{@grid[:a1]}  |  #{@grid[:a2]}  |  #{@grid[:a3]} "
+    15.times {print "-"}
+    puts
+    puts " #{@grid[:b1]}  |  #{@grid[:b2]}  |  #{@grid[:b3]} "
+    15.times {print "-"}
+    puts
+    puts " #{@grid[:c1]}  |  #{@grid[:c2]}  | #{@grid[:c3]} "
   end
 end
