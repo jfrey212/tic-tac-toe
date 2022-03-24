ca64efee61df7600d0125c474314fadf0e94a533
jfrey212 <eojyerf@gmail.com>
2022-03-23 21:12:25 -0500

    Building out Game class - added name_players and player_move methods

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -4,11 +4,31 @@
 # The Game class has methods for starting and finishing the game, and records number of games played.
 
 class Game
-  attr_reader :number_of_games, :name
+  attr_accessor :player1, :player2, :current_turn
+  attr_reader :name
 
-  def initialize; end
+  @@game_count = 0
 
-  def player_move; end
+  def initialize
+    @name = 'Tic-Tac-Toe'
+    @turn_count = 1
+    @@game_count += 1
+  end
+
+  def name_players
+    print 'Player 1 Enter Name: '
+    @player1 = Player.new(gets.chomp)
+    puts
+    print 'Player 2 Enter Name: '
+    @player2 = Player.new(gets.chomp)
+    puts [@player1, @player2]
+    self.current_turn = self.player1
+  end
+
+  def player_move(board)
+    print "#{player1} pick a box: "
+    board.add_x(gets.chomp.to_sym)
+  end
 
   def add_player_move; end
 end
@@ -30,14 +50,15 @@ class Board
     @grid[cell] = 'O'
   end
 
-  def display_board
-    puts " #{@grid[:a1]}  |  #{@grid[:a2]}  |  #{@grid[:a3]} "
-    15.times { print '-' }
+  def display
+    puts '     a    b     c    '
+    puts " 1  #{@grid[:a1]}  |  #{@grid[:a2]}  |  #{@grid[:a3]} "
+    print '   ----------------'
     puts
-    puts " #{@grid[:b1]}  |  #{@grid[:b2]}  |  #{@grid[:b3]} "
-    15.times { print '-' }
+    puts " 2  #{@grid[:b1]}  |  #{@grid[:b2]}  |  #{@grid[:b3]} "
+    print '   ----------------'
     puts
-    puts " #{@grid[:c1]}  |  #{@grid[:c2]}  | #{@grid[:c3]} "
+    puts " 3  #{@grid[:c1]}  |  #{@grid[:c2]}  | #{@grid[:c3]} "
   end
 end
 
@@ -46,7 +67,9 @@ end
 class Player
   attr_accessor :name
 
-  def initialize; end
+  def initialize(name)
+    self.name = name
+  end
 end
 
 # Instances of the Turn class pass user turn data to the board instance.
