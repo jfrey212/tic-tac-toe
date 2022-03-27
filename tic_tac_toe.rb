cdd72e67fd76ef37641a0c8c46cfbc7ecb2c62e9
jfrey212 <eojyerf@gmail.com>
2022-03-26 23:37:03 -0500

    Building out Game class

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,15 +1,17 @@
-
 # Odin Project Tic Tac Toe Game
 # by Joseph Frey
 
 # The Game class has methods for starting and finishing the game, and records number of games played.
 
 class Game
-  attr_accessor :player1, :player2, :current_turn, :turn_count, :board
-
   def initialize
-    self.turn_count = 0
-    self.board = Board.new
+    @player1 = ''
+    @player2 = ''
+    @is_winner = false
+    @winner = ''
+    @current_turn = 0
+    @turn_count = 1
+    @board = Board.new
   end
 
   def name_players
@@ -21,29 +23,59 @@ class Game
   end
 
   def next_turn
-    system("clear")
-    puts current_turn
-    self.board.display
-    if self.current_turn == 0
-      print "#{player1.name} pick a box: "
-      board.add_x(gets.chomp.to_sym)
-      self.current_turn = 1
-    elsif self.current_turn == 1
-      print "#{player2.name} pick a box: "
-      board.add_o(gets.chomp.to_sym)
-      self.current_turn = 0
+    system('clear')
+    puts "Turn #{@turn_count}"
+    @board.display
+    if @current_turn.zero?
+      print "\n#{@player1.name} pick a box: "
+      @board.add_x(gets.chomp.to_sym)
+      @current_turn = 1
+      @turn_count += 1
+    elsif @current_turn == 1
+      print "\n#{@player2.name} pick a box: "
+      @board.add_o(gets.chomp.to_sym)
+      @current_turn = 0
+      @turn_count += 1
     end
   end
 
-  def start_game
-    name_players
-    self.current_turn = 0
+  def victory_check
+    if @board.grid[:a1] == @board.grid[:a2] && @board.grid[:a2] == @board.grid[:a3]
+      @is_winner = true
+      @winner = @board.grid[:a1} == 'X' ? @player1 : @player2
+    elsif @board.grid[:b1] == @board.grid[:b2] && @board.grid[:b2] == @board.grid[:b3]
+      @is_winner = true
+      @winner = @board.grid[:b1} == 'X' ? @player1 : @player2
+    elsif @board.grid[:c1] == @board.grid[:c2] && @board.grid[:c2] == @board.grid[:c3]
+      @is_winner = true
+      @winner = @board.grid[:c1} == 'X' ? @player1 : @player2
+    elsif @board.grid[:a1] == @board.grid[:b1] && @board.grid[:b1] == @board.grid[:c1]
+      @is_winner = true
+      @winner = @board.grid[:a1} == 'X' ? @player1 : @player2
+    elsif @board.grid[:a2] == @board.grid[:b2] && @board.grid[:b2] == @board.grid[:c2]
+      @is_winner = true
+      @winner = @board.grid[:a2} == 'X' ? @player1 : @player2
+    elsif @board.grid[:a3] == @board.grid[:b3] && @board.grid[:b3] == @board.grid[:c3]
+      @is_winner = true
+      @winner = @board.grid[:a3} == 'X' ? @player1 : @player2
+    elsif @board.grid[:a1] == @board.grid[:b2] && @board.grid[:b2] == @board.grid[:c3]
+      @is_winner = true
+      @winner = @board.grid[:a1} == 'X' ? @player1 : @player2
+    elsif @board.grid[:a3] == @board.grid[:b2] && @board.grid[:b2] == @board.grid[:c1]
+      @is_winner = true
+      @winner = @board.grid[:a3} == 'X' ? @player1 : @player2
+    else
+      @is_winner = false
+    end
+    puts @is_winner
+    puts @winner
   end
 end
 
 # The board class draws the tic-tac-toe board and can add X or O to a cell in the board.
 
 class Board
+  attr_reader :grid
   def initialize
     @grid = { a1: ' ', a2: ' ', a3: ' ',
               b1: ' ', b2: ' ', b3: ' ',
@@ -68,6 +100,10 @@ class Board
     puts
     puts " c  #{@grid[:c1]}  |  #{@grid[:c2]}  | #{@grid[:c3]} "
   end
+
+  private
+
+  attr_writer :grid
 end
 
 # Player instances hold a player's name and has a method for playing a turn
@@ -85,7 +121,3 @@ end
 class Turn
   def initialize; end
 end
-
-g = Game.new
-g.start_game
-g.next_turn
